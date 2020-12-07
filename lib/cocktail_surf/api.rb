89a8abd02e38f2f2c667cc2a9cfcda62d1ef562e
TimmmyD <tmd112593@gmail.com>
2020-12-07 15:35:28 -0800

    Added attributes to the cocktail class and created an array for the liquors that are being chosen from the user

--- lib/cocktail_surf/api.rb
+++ lib/cocktail_surf/api.rb
@@ -1,1 +1,10 @@
-class API
+class Cocktails::API
+
+
+    https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin
+
+    https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka
+
+    https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Whiskey
+
+    https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Tequila
