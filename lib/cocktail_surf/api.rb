75ca28ac5a104690892e19634c35980f45a783a8
TimmmyD <tmd112593@gmail.com>
2020-12-10 13:33:01 -0800

    Lists drinks retrieved from API

--- lib/cocktail_surf/api.rb
+++ lib/cocktail_surf/api.rb
@@ -1,5 +1,23 @@
-class Cocktail::API
+require 'httparty'
 
+
+class API
+
+
+    def self.find_cocktails(ingredient)
+        url = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=#{ingredient}"
+        response = HTTParty.get(url)
+        #puts response.body
+        drinks_hash = JSON.parse(response.body)
+        drinks_array = drinks_hash["drinks"]
+        drinks_array.each do |hash|
+            puts hash["strDrink"]
+        end
+        
+        
+        
+        #puts drinks_array.first["strDrink"]
+    end
 
     #https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin
 
